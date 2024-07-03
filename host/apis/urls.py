52fa21e53612b486c9265d2446610990a6564753
RadwanHegazy <radwangaber22@gmail.com>
2024-07-03 22:35:03 +0300

    write some updates

--- host/apis/urls.py
+++ host/apis/urls.py
@@ -3,7 +3,8 @@ from .views import get, create, patch
 
 urlpatterns = [
     path('create/', create.create_host_view.as_view()),
-    path('get/<str:host_name>/', get.get_host_view.as_view()),
+    path('get/', get.get_host_list.as_view()),
+    path('get/<str:host_name>/<str:file_name>/', get.get_host_view.as_view()),
     path('active/<int:host_id>/', patch.host_active_view.as_view()),
     path('inactive/<int:host_id>/', patch.host_inactive_view.as_view())
 ]
